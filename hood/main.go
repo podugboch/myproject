a70d6e6007c11fb32549235158b17a23132f9623
podugboch <odu.pisc@gmail.com>
2025-06-03 12:18:26 +0100

    hood/main.go

--- hood/main.go
+++ hood/main.go
@@ -19,17 +19,17 @@ func createProfile(name string, age int, height float64, occupation string, isMa
 
 func viewProfile(p Profile) {
 
-	fmt.Println("Name:", p.name)
-	fmt.Println("Age:", p.age)
-	fmt.Println("Height:", p.height)
-	fmt.Println("Occupation:", p.occupation)
-	fmt.Println("Married:", p.isMarried)
-	fmt.Println("Student:", p.isStudent)
-	fmt.Println("Nationality:", p.nationality)
-	fmt.Println("State:", p.state)
+	fmt.Printf("%-15s %s\n", "Name:", p.name)
+	fmt.Printf("%-15s %d\n", "Age:", p.age)
+	fmt.Printf("%-15s %.1f\n", "Height:", p.height)
+	fmt.Printf("%-15s %s\n", "Occupation:", p.occupation)
+	fmt.Printf("%-15s %v\n", "Married:", p.isMarried)
+	fmt.Printf("%-15s %v\n", "Student:", p.isStudent)
+	fmt.Printf("%-15s %s\n", "Nationality:", p.nationality)
+	fmt.Printf("%-15s %s\n", "State:", p.state)
 }
 
 func main() {
-	Profile := createProfile("Patrick Odugboche", 29, 5.9, "Programmer", true, false, "Nigerian", "Kwararafa")
+	Profile := createProfile("Patrick Sean Odugboche", 29, 5.9, "Programmer", true, false, "Nigerian", "Kwararafa")
 	viewProfile(Profile)
 }
